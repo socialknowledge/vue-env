1404e769e0447c5734f511efa54c422342db9125
Jeff Capron <jeff@socialknowledge.com>
2020-06-15 17:36:02 +0100

    Cleanup

--- src/Env.php
+++ src/Env.php
@@ -6,6 +6,6 @@ class Env {
 
     public static function outputEnv()
     {
-        return view('vue-env::vue-env', ['object_name' => config('vue-env.object_name'), 'data' => config('vue-env.data')])->render();
+        return view('vue-env.vue-env', ['object_name' => config('vue-env.object_name'), 'data' => config('vue-env.data')])->render();
     }
 }
--- src/views/vue-env.blade.php
+++ src/views/vue-env.blade.php
@@ -1,7 +1,7 @@
-<script type="javascript">
+<script>
     window.{{ $object_name }} = {};
     @foreach($data as $key => $value)
-        window.{{ $object_name }}.{{ $key  }} = {!! json_encode($value) !!}
+        window.{{ $object_name }}.{{ $key  }} = '{!! json_encode($value) !!}'
     @endforeach
 </script>
 
