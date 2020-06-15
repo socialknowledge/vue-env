2857e31dc4d70ba54aa4c6b6afeb014072e8dae5
Jeff Capron <jeff@socialknowledge.com>
2020-06-15 17:59:11 +0100

    version

--- src/views/vue-env.blade.php
+++ src/views/vue-env.blade.php
@@ -1,7 +1,7 @@
 <script>
     window.{{ $object_name }} = {};
     @foreach($data as $key => $value)
-        window.{{ $object_name }}.{{ $key  }} = '{!! json_encode($value) !!}'
+        window.{{ $object_name }}.{{ $key  }}=@if (is_bool($value))'{!! json_encode($value) !!}'@elseif(is_int($value) || is_float($value)){{ $value }}@else {!! json_encode($value) !!}@endif;
     @endforeach
 </script>
 
